61a123fe7117c53390db2077347282db40eab5be
Steven Giang <17sgiang@gmail.com>
2021-04-03 07:40:49 -0400

    Able to call web API, doesn't change bookList

--- app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
+++ app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
@@ -3,6 +3,11 @@ package edu.temple.bookshelf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,6 +16,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class BookSearchActivity extends AppCompatActivity {
@@ -22,20 +28,32 @@ public class BookSearchActivity extends AppCompatActivity {
     // Results of search should be returned to the main activity (Intent)
     // When the user performs a search, the application must always display the BookListFragment
 
+    EditText searchBox;
+    Button cancelButton, searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_search);
 
-        // TODO make an onclicklistener for the button that calls callAPI
+        searchBox = findViewById(R.id.search_box);
+        cancelButton = findViewById(R.id.cancel_button);
+        searchButton = findViewById(R.id.accept_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("MyTag", "Button Clicked");
+                Log.d("MyTag", searchBox.getText().toString());
+                callAPI(searchBox.getText().toString());
+            }
+        });
 
     }
 
-    private JSONObject[] callAPI(String args){
+    private JSONArray callAPI(String args){
+        Log.d("MyTag", "Entered callAPI()");
 
-        JSONObject[] books;
-
-        // TODO Process the response
 
         // Instantiate the RequestQueue
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -46,16 +64,24 @@ public class BookSearchActivity extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 // On success, turn String to JSON object
-
+                Log.d("MyTag", response);
+                try{
+                    JSONArray books = new JSONArray(response);
+                    // TODO Pass books to the outer method
+                } catch (Exception e) {
+                    // Log error
+                }
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error){
                 // Error message
+
             }
         });
 
+        queue.add(stringRequest);
         // Error
         return null;
     }
